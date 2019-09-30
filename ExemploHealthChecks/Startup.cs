3a85f5e9b570999ef55b8d2c03a32c35c2d120a7
Fernando Mendes <fernando.mendes@xpi.com.br>
2019-09-30 10:03:41 -0300

    Novo

---
+++ ExemploHealthChecks/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Confluent.Kafka;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ExemploHealthChecks
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var configKafka = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            var connectionMongo = "mongodb://localhost:17017";
+            var enderecoExemplo2 = "http://localhost:50993/api/values";
+
+            services.AddHealthChecks()
+                .AddMongoDb(connectionMongo, name: "mongoDb")
+                .AddKafka(configKafka, name: "kafka")
+                .AddUrlGroup(new Uri(enderecoExemplo2), name: "exemploHealthChecks2");
+
+
+            services.AddHealthChecksUI();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Gera o endpoint que retornará os dados utilizados no dashboard
+            app.UseHealthChecks("/health-marraia", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            // Ativa o dashboard para a visualização da situação de cada Health Check
+            app.UseHealthChecksUI();
+
+            app.UseMvc();
+        }
+    }
+}
